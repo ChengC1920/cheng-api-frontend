56674771e1a6a7caf74c9203c01e92064756b05c
ichensw <2480311255@qq.com>
2023-06-08 09:29:14 +0800

    初始化项目

---
+++ src/services/nero-api-backend/interfaceInfoController.ts
@@ -0,0 +1,78 @@
+// @ts-ignore
+/* eslint-disable */
+import { request } from '@umijs/max';
+
+/** addInterfaceInfo POST /api/interfaceInfo/add */
+export async function addInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoAddRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseLong_>('/api/interfaceInfo/add', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** deleteInterfaceInfo POST /api/interfaceInfo/delete */
+export async function deleteInterfaceInfoUsingPOST(
+  body: API.DeleteRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/interfaceInfo/delete', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** getInterfaceInfoVOById GET /api/interfaceInfo/get/vo */
+export async function getInterfaceInfoVOByIdUsingGET(
+  // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
+  params: API.getInterfaceInfoVOByIdUsingGETParams,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseInterfaceInfoVO_>('/api/interfaceInfo/get/vo', {
+    method: 'GET',
+    params: {
+      ...params,
+    },
+    ...(options || {}),
+  });
+}
+
+/** listInterfaceInfoVOByPage POST /api/interfaceInfo/list/page/vo */
+export async function listInterfaceInfoVOByPageUsingPOST(
+  body: API.InterfaceInfoQueryRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponsePageInterfaceInfoVO_>('/api/interfaceInfo/list/page/vo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
+
+/** updateInterfaceInfo POST /api/interfaceInfo/update */
+export async function updateInterfaceInfoUsingPOST(
+  body: API.InterfaceInfoUpdateRequest,
+  options?: { [key: string]: any },
+) {
+  return request<API.BaseResponseBoolean_>('/api/interfaceInfo/update', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    data: body,
+    ...(options || {}),
+  });
+}
---
+++ src/services/nero-api-backend/typings.d.ts
@@ -0,0 +1,281 @@
+declare namespace API {
+  type BaseResponseBoolean_ = {
+    code?: number;
+    data?: boolean;
+    message?: string;
+  };
+
+  type BaseResponseInterfaceInfoVO_ = {
+    code?: number;
+    data?: InterfaceInfoVO;
+    message?: string;
+  };
+
+  type BaseResponseLoginUserVO_ = {
+    code?: number;
+    data?: LoginUserVO;
+    message?: string;
+  };
+
+  type BaseResponseLong_ = {
+    code?: number;
+    data?: number;
+    message?: string;
+  };
+
+  type BaseResponsePageInterfaceInfoVO_ = {
+    code?: number;
+    data?: PageInterfaceInfoVO_;
+    message?: string;
+  };
+
+  type BaseResponsePageUser_ = {
+    code?: number;
+    data?: PageUser_;
+    message?: string;
+  };
+
+  type BaseResponsePageUserVO_ = {
+    code?: number;
+    data?: PageUserVO_;
+    message?: string;
+  };
+
+  type BaseResponseString_ = {
+    code?: number;
+    data?: string;
+    message?: string;
+  };
+
+  type BaseResponseUser_ = {
+    code?: number;
+    data?: User;
+    message?: string;
+  };
+
+  type BaseResponseUserVO_ = {
+    code?: number;
+    data?: UserVO;
+    message?: string;
+  };
+
+  type checkUsingGETParams = {
+    /** echostr */
+    echostr?: string;
+    /** nonce */
+    nonce?: string;
+    /** signature */
+    signature?: string;
+    /** timestamp */
+    timestamp?: string;
+  };
+
+  type DeleteRequest = {
+    id?: number;
+  };
+
+  type getInterfaceInfoVOByIdUsingGETParams = {
+    /** id */
+    id?: number;
+  };
+
+  type getUserByIdUsingGETParams = {
+    /** id */
+    id?: number;
+  };
+
+  type getUserVOByIdUsingGETParams = {
+    /** id */
+    id?: number;
+  };
+
+  type InterfaceInfoAddRequest = {
+    description?: string;
+    method?: string;
+    name?: string;
+    requestHeader?: string;
+    requestParams?: string;
+    responseHeader?: string;
+    status?: number;
+    url?: string;
+  };
+
+  type InterfaceInfoQueryRequest = {
+    current?: number;
+    description?: string;
+    id?: number;
+    isDelete?: number;
+    method?: string;
+    name?: string;
+    pageSize?: number;
+    requestHeader?: string;
+    requestParams?: string;
+    responseHeader?: string;
+    searchText?: string;
+    sortField?: string;
+    sortOrder?: string;
+    status?: number;
+    url?: string;
+    userId?: number;
+  };
+
+  type InterfaceInfoUpdateRequest = {
+    description?: string;
+    id?: number;
+    method?: string;
+    name?: string;
+    requestHeader?: string;
+    requestParams?: string;
+    responseHeader?: string;
+    status?: number;
+    url?: string;
+  };
+
+  type InterfaceInfoVO = {
+    createTime?: string;
+    description?: string;
+    id?: number;
+    method?: string;
+    name?: string;
+    requestHeader?: string;
+    requestParams?: string;
+    responseHeader?: string;
+    status?: number;
+    updateTime?: string;
+    url?: string;
+    user?: UserVO;
+    userId?: number;
+  };
+
+  type LoginUserVO = {
+    createTime?: string;
+    id?: number;
+    updateTime?: string;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type OrderItem = {
+    asc?: boolean;
+    column?: string;
+  };
+
+  type PageInterfaceInfoVO_ = {
+    countId?: string;
+    current?: number;
+    maxLimit?: number;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: number;
+    records?: InterfaceInfoVO[];
+    searchCount?: boolean;
+    size?: number;
+    total?: number;
+  };
+
+  type PageUser_ = {
+    countId?: string;
+    current?: number;
+    maxLimit?: number;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: number;
+    records?: User[];
+    searchCount?: boolean;
+    size?: number;
+    total?: number;
+  };
+
+  type PageUserVO_ = {
+    countId?: string;
+    current?: number;
+    maxLimit?: number;
+    optimizeCountSql?: boolean;
+    orders?: OrderItem[];
+    pages?: number;
+    records?: UserVO[];
+    searchCount?: boolean;
+    size?: number;
+    total?: number;
+  };
+
+  type uploadFileUsingPOSTParams = {
+    biz?: string;
+  };
+
+  type User = {
+    createTime?: string;
+    id?: number;
+    isDelete?: number;
+    mpOpenId?: string;
+    unionId?: string;
+    updateTime?: string;
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userPassword?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserAddRequest = {
+    userAccount?: string;
+    userAvatar?: string;
+    userName?: string;
+    userRole?: string;
+  };
+
+  type userLoginByWxOpenUsingGETParams = {
+    /** code */
+    code: string;
+  };
+
+  type UserLoginRequest = {
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserQueryRequest = {
+    current?: number;
+    id?: number;
+    mpOpenId?: string;
+    pageSize?: number;
+    sortField?: string;
+    sortOrder?: string;
+    unionId?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserRegisterRequest = {
+    checkPassword?: string;
+    userAccount?: string;
+    userPassword?: string;
+  };
+
+  type UserUpdateMyRequest = {
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+  };
+
+  type UserUpdateRequest = {
+    id?: number;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+
+  type UserVO = {
+    createTime?: string;
+    id?: number;
+    userAvatar?: string;
+    userName?: string;
+    userProfile?: string;
+    userRole?: string;
+  };
+}
